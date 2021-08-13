81d04dd1098ab3f117a42b86b11a91d46dbdf6a3
mauricio178 <mauricio.log98@hotmail.com>
2021-08-13 16:56:28 -0300

    Implement Prismic and Pull in a List

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,27 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react'
+import { ActiveLink } from '../ActiveLink/indes'
 import { ButtonSignIn } from '../ButtonSignIn'
 import styles from './styles.module.scss'
 
 export function Header() {
+
+
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
-                <img src="/images/ig.new.png" alt="ig.news" width="170" height="70"/>
+                <img src="/images/ig.new.png" alt="ig.news" width="170" height="70" />
                 <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
+                    <ActiveLink activeClassName= {styles.active} href="/" >
+                        <a>Home</a>
+                    </ActiveLink>
+
+                    <ActiveLink  activeClassName= {styles.active} href="/posts" >
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
 
-                <ButtonSignIn/>
+                <ButtonSignIn />
             </div>
         </header>
     );
